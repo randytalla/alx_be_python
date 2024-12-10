607c94bf23a14310137f64e00838f515a37ef3cd
RandyTalla <talarandy2@gmail.com>
2024-12-10 05:45:43 +0100

    little modification

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
-P = 1000
-R = 0.05
-T = 3
-I = P * R * T
-print("The simple interest is:", I)
+principle = 1000
+rate = 0.05
+time = 3
+simple_interest = principle * rate * time
+print("The simple interest is:", simple_interest)
