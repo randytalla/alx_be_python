d4d39071bd1afe59c28aad34b1763e845a0adf1d
RandyTalla <talarandy2@gmail.com>
2025-01-08 13:14:37 +0100

    implemented the code of function for +,-,/,-

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        try:
+            return num1 / num2
+        except ZeroDivisionError:
+            return "Error: Division by zero is not allowed"
+        else:
+            return "Error: Invalid operation"
+
+
+# if __name__ == "__main__":
+#     result = perform_operation(10,0,"divide")
+#     print(f"Result: {result}")
