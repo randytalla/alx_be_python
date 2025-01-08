0ed055d16593ec3d0fee647efecac80c66ccec19
RandyTalla <talarandy2@gmail.com>
2025-01-08 18:48:49 +0100

    created fns_and_dsa directory and the arithmetic_operations.py file and implemented the neccessary code

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,19 +1,20 @@
 def perform_operation (num1:float, num2:float, operation:str):
-    if operation == "add":
-        return num1 + num2
-    elif operation == "subract":
-        return num1 - num2
-    elif operation == "multiply":
-        return num1 * num2
-    elif operation == "divide":
-        try:
-            return num1 / num2
-        except ZeroDivisionError:
-            return "Error: Division by zero is not allowed"
-    else:
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 != 0:
+                return num1 / num2
+            else:
+                return "Error: Division by zero is not allowed"
+        case _:
             return "Error: Invalid operation"
 
 
-# if __name__ == "__main__":
-#     result = perform_operation(0,10,"divide")
-#     print(f"Result: {result}")
+if __name__ == "__main__":
+    result = perform_operation(10,0,"divide")
+    print(f"Result: {result}")
