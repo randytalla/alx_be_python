26e8813cac578ea21baa0ad5e9be043ad34a9496
RandyTalla <talarandy2@gmail.com>
2025-01-08 13:24:48 +0100

    created the fns_and_dsa directory and the arithmetic_operations.py file and implemented the neccessary code

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation (num1: float, num2: float, operation: str):
     if operation == "add":
         return num1 + num2
     elif operation == "subract":
@@ -15,5 +15,5 @@ def perform_operation(num1: float, num2: float, operation: str):
 
 
 # if __name__ == "__main__":
-#     result = perform_operation(10,0,"divide")
+#     result = perform_operation(0,10,"divide")
 #     print(f"Result: {result}")
