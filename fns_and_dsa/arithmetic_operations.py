8b52d25c284490695dc588f5668fd80dec263dd7
RandyTalla <talarandy2@gmail.com>
2025-01-08 18:49:46 +0100

    created fns_and_dsa directory and the arithmetic_operations.py file and implemented the neccessary code

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -15,6 +15,6 @@ def perform_operation (num1:float, num2:float, operation:str):
             return "Error: Invalid operation"
 
 
-if __name__ == "__main__":
-    result = perform_operation(10,0,"divide")
-    print(f"Result: {result}")
+# if __name__ == "__main__":
+#     result = perform_operation(10,0,"divide")
+#     print(f"Result: {result}")
