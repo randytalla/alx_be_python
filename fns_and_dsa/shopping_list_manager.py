a88d3dc1845aa38c032df23e036e8d8440f77732
RandyTalla <talarandy2@gmail.com>
2025-01-11 12:07:02 +0100

    implemented a shoping cart using list control structure

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,40 +1,33 @@
 def display_menu():
-    print("Shopping list Manager")
+    print(f"Shopping list Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View Item")
     print("4. Exit")
 
 def main():
-    Shopping_list = []
+    shopping_list = []
     while True:
         display_menu()
         choice = input("Enter your choice: ")
-        
-        if not choice.isdigit(): 
-            print("Invalid choice. Please enter a number.") 
-            continue
 
-        choice = int(choice)
-
-
-        if choice == "1":
+        if choice == '1':
             item = input ("Enter the item to add: ").strip()
-            Shopping_list.append(item)
-            print("Item added to the list")
-        elif choice == "2":
-            item = input("Enter the item to remove: ")
-            try:
-                Shopping_list.remove(item)
-                print("Item removed from the list")
-            except ValueError:
-                print("Item not found in the list")
+            shopping_list.append(item)
+            print(f"{item} has been added to the shopping list")
+        elif choice == '2':
+            item = input("Enter the item to remove: ").strip()
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item} has been removed from the shopping list")
+            else:
+                print(f"{item} is not in the shopping list")
         elif choice == "3":
             print("Shopping list items:")
-            for item in Shopping_list:
+            for item in shopping_list:
                 print(item)
         elif choice == "4":
-            print("Exiting the program")
+            print("Goodbye!")
             break
         else:
             print("invalid value")
