07eef26b6a421453405968e249dcf626c0f4ef14
RandyTalla <talarandy2@gmail.com>
2025-01-17 04:36:15 +0100

    created my directory, and bank account.py file. implmenented the code for deposit, withdraw and display balance inthe class Bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, initial_balance = 0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            print("Insufficient funds!")
+        else:
+            self.account_balance -= amount
+            return self.account_balance
+
+    def display_balance():
+        print(f"Your curent balance {self.account_balance}")
+
