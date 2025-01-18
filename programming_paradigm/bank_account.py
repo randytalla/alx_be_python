19ddab395c671ee86b8d0287772cff01a5ffec8f
RandyTalla <talarandy2@gmail.com>
2025-01-18 18:03:29 +0100

    Resolve the withdrawal output error

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,13 +7,18 @@ class BankAccount:
 
     def withdraw(self, amount):
         if amount > self.account_balance:
-            print("Insufficient funds.")
-        elif amount <= 0: 
-            print("Withdrawal amount must be positive.")
-        else:
+            return False
+        elif amount > 0: 
             self.account_balance -= amount
-            return self.account_balance
+            return True
+        else:
+            print("Withdrawal amount must be positive.")
+            return False
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
+
+
+
+
 
