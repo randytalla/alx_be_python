f8453085eebe0d16de054709459c4156ba3e1b88
RandyTalla <talarandy2@gmail.com>
2025-01-18 03:45:46 +0100

    understanding of basic OOP concepts in Python by implementing a system that tracks books in a library, focusing on classes, object instantiation, and method invocation.

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,59 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+        self.__is_checked_out = False
+
+    def check_out(self):
+        if not self.__is_checked_out:
+            self.__is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self.__is_checked_out:
+            self.__is_checked_out = False
+            return True
+        return False
+    
+    def is_available(self):
+        return not self.__is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+    def add_book(self, book):
+        self.books.append(book)
+
+    def  check_out_book(self, title):
+        for book in self.__books:
+            if book.title == title:
+                if book.check_out():
+                    print(f"Book {title} returned successfully.")
+                else:
+                    print(f"Book {title} is already checked out.")
+                return
+        print(f"Book '{title}' not found in the library.")
+
+
+    def return_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                if book.return_book():
+                    print(f"Book '{title}' returned successfully.")
+                else:
+                    print(f"Book '{title}' is not checked out.")
+                return
+            print(f"Book '{title}' not found in the library.")
+
+    def list_available_books(self):
+        available_books = [book for book in self.__books if book.is_available()] 
+        if available_books: 
+            for book in available_books: 
+                print(f"{book.title} by {book.author}")
+        else: print("No books available.")
+    
+            
+            
