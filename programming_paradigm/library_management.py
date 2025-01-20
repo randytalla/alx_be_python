cc28bca9e35fe08fb048eb29d37bbe75dc5fdd9d
RandyTalla <talarandy2@gmail.com>
2025-01-20 12:18:25 +0100

    corrected the add, list_available and return book method

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -24,27 +24,27 @@ class Book:
 
 class Library:
     def __init__(self):
-        self.__books = []
+        self._books = []
 
     def add_book(self, book):
-        self.__books.append(book)
+        self._books.append(book)
 
     def check_out_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title and book.check_out():
                 print(f"Checked out {book}")
                 return
         print(f"Sorry, {title} is not available")
 
     def return_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title and book.return_book():
                 print(f"Returned: '{book}'")
                 return
         print(f"Sorry, '{title}' was not checked out or does not exist.")
 
     def list_available_books(self):
-        available_books = [book for book in self.__books if book.is_available()]
+        available_books = [book for book in self._books if book.is_available()]
         if available_books:
             for book in available_books:
                 print(book)
