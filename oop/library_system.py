f54adfbdb6948464a6a9969d3f476af72638903d
RandyTalla <talarandy2@gmail.com>
2025-01-25 04:18:11 +0100

    Deepen your understanding of inheritance and composition in Python by creating a system that models a library with different types of books.

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,12 +3,34 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f'{self.title} by {self.author}'
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        return f"EBook('{self.title}', '{self.author}', '{self.file_size}')"
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook('{self.title}', '{self.author}', {self.page_count} pages)"
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+        print(f"Book '{book.title}' added to library.")
+    def list_books(self):
+        for book in self.books:
+            print(f"{book.title} by {book.author}")
+
+    
