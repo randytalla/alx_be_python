89ad57b0718f26aa078698be2da45a411f00d96a
RandyTalla <talarandy2@gmail.com>
2025-01-25 05:08:35 +0100

    Enhance your understanding of polymorphism in Python by creating a set of classes that demonstrate method overriding and polymorphic behavior using shape, rectangle, and circle.

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,19 @@
+class Shape:
+    def area(self):
+        raise NotImplementedError("Derived classes need to override this method.")
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+
+    def area(self):
+        return 3.14159 * self.radius ** 2
